80f8d4f86c6ba67da911421d79c43217964d75dd
Sharnitha VijayaKumar <96509382+Sharnitha@users.noreply.github.com>
2025-04-07 15:36:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,16 @@ export default defineConfig({
       'practicecurrentcontainerapptest.orangestone-98956164.eastus.azurecontainerapps.io',
     ],
   },
+  server: {
+    // this ensures that the browser opens upon server start
+    open: false,
+    // this sets a default port to 3000
+    watch: {
+      usePolling: true, // Enable polling to detect file changes
+      interval: 100,    // Adjust the polling interval if needed
+    },
+    host: true,          // Allow access from outside the container
+    port: 3000,          // Match the port in your Docker setup
+    strictPort: true,    // Ensure the port is not changed
+  }
 });
